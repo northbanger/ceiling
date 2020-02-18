9e9d6843bd3c575e17e2b82aab3ec6db10944074
dword <sebastianlandw@gmail.com>
2020-02-18 22:18:56 +0000

    fix: fixes after upgrades

--- test/push/no-plugins.test.js
+++
@@ -1,7 +0,0 @@
-import { spawn } from 'child-process-promise'
-import withLocalTmpDir from 'with-local-tmp-dir'
-
-export default () => withLocalTmpDir(__dirname, async () => {
-  const { stdout } = await spawn('ceiling', ['push', '-y'], { capture: ['stdout'] })
-  expect(stdout).toEqual('No plugins specified. Doing nothing …\n')
-})
